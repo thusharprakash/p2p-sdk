3b7995b8ed524c8b7f691a428595b0836866ee74
athulantony95 <154115716+athulantony95@users.noreply.github.com>
2024-06-07 10:37:56 +0530

    iOS support

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -1,8 +1,8 @@
-rm -rf p2p/out
+rm -rf p2p/out/android
 go get golang.org/x/mobile/cmd/gomobile
 go mod download
 go run golang.org/x/mobile/cmd/gomobile init
 GO111MODULE=on
 cd p2p/
-mkdir out
-go run golang.org/x/mobile/cmd/gomobile bind -v -target=android -o out/p2p.aar  -androidapi 24
+mkdir out/android
+go run golang.org/x/mobile/cmd/gomobile bind -v -target=android -o out/android/p2p.aar  -androidapi 24
