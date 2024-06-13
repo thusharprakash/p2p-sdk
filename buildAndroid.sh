718d312fc8c22f3a23cf73cfabdb246fae5b85cf
Thushar M Prakash <thushar@arezlabs.com>
2024-06-13 10:02:45 +0700

    Build script updated

--- buildAndroid.sh
+++ buildAndroid.sh
@@ -17,8 +17,9 @@ go mod download
 go run golang.org/x/mobile/cmd/gomobile init
 GO111MODULE=on
 cd p2p/
-mkdir out/android
+mkdir -p out/android
 go run golang.org/x/mobile/cmd/gomobile bind -v -target=android -o out/android/p2p.aar  -androidapi 24
+mkdir -p ../MobileApp/android/app/libs
 cp out/android/p2p.aar ../MobileApp/android/app/libs/
 cd ..
 cd MobileApp/android
